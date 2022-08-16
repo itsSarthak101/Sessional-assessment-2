b73be50682cbfb5ca6e18b0fecc0a27925fab206
itsSarthak101 <sarthak120202@gmail.com>
2022-08-16 19:05:27 +0530

    api bug fix

--- ajax.js
+++ ajax.js
@@ -1,15 +1,19 @@
 // Get Sample Images
+
+window.location.reload = () => {}
 function getPhotos() {
     console.log("It is Working");
     $.get('https://jsonplaceholder.typicode.com/photos',function(data){
         for(let i=0;i<100;i++){
             var photo = data[i].url;
+            console.log(data[i])
+            console.log("sarthak");
             $("#image_container").append(`<img src="${photo}"/>`);
         }
     }).fail(function(xhr,textStatus,errorThrown){
         console.log("Error: " + errorThrown)
     })
-}
+};
 
 $("#search_btn").click(getPhotos);
 
@@ -19,3 +23,6 @@ $("#post_blogt").click(function() {
     var inpBlog = $("#inputblog").val();
     $("#blog-col").append(`<div id="bodyBlog"></div><div id="blogUser">${user}</div><div id="blogContent">${inpBlog}</div></div>`)
 });
+
+
+
